aad5696240662fa0b5eac881bc379b39cd48c0ae
Qabas al ani <doctorqa@yahoo.com>
2021-08-16 09:25:24 -0400

    i refactored my public and db files so that it will be much easier to use them outside of the develop folder that i deleted because it was no needed anymore

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
-const express = require('express');
-const fs = require('fs');
+const express = require("express");
+const fs = require("fs");
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
