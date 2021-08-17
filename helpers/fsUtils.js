bf0c359dc1a429b4623494639dca04649ad2cfed
Qabas al ani <doctorqa@yahoo.com>
2021-08-17 13:42:30 -0400

    added comments to fsUtils.js that makes the readablity easier

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,15 +1,18 @@
-
+//  requiring fs and util moduals
 const fs = require("fs");
 const util = require("util");
 
+// creating read file for the fs
 const readFromFile = util.promisify(fs.readFile);
 
+// having write file with the arguments of destinationand content
 const writeToFile = (destination, content) => {
   fs.writeFile(destination, JSON.stringify(content, null, 4), err =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 };
 
+// creating a function that writes an error if found or to append the right results
 const readAndAppend = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
@@ -22,4 +25,6 @@ const readAndAppend = (content, file) => {
   });
 };
 
+
+// exporting the modules
 module.exports = { readFromFile, writeToFile, readAndAppend };
